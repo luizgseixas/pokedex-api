7b28ee02bcaa50b806d6b301491fa602a7f03dcc
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-06-28 20:45:02 -0300

    code formatt

--- src/presentation/controllers/get-family-tree.controller.ts
+++ src/presentation/controllers/get-family-tree.controller.ts
@@ -1,24 +1,23 @@
-import { Request, Response } from "express";
-import { IController } from "src/domain/controller/controller";
-import { IFamilyTree } from "src/domain/interfaces/interfaces";
-import { IMapFamilyTreeService } from "src/domain/services/map-family-tree.service";
-import { MapFamilyTreeService } from '../../application/services/map-family-tree.service'
+import { Request, Response } from 'express';
+import { IController } from 'src/domain/controller/controller';
+import { IFamilyTree } from 'src/domain/interfaces/interfaces';
+import { IMapFamilyTreeService } from 'src/domain/services/map-family-tree.service';
 
 export class GetFamilyTreeController implements IController<IFamilyTree> {
-  private readonly getFamilyTreeService: IMapFamilyTreeService
+  private readonly getFamilyTreeService: IMapFamilyTreeService;
 
-  constructor(getFamilyTreeService: IMapFamilyTreeService){
-    this.getFamilyTreeService = getFamilyTreeService
+  constructor(getFamilyTreeService: IMapFamilyTreeService) {
+    this.getFamilyTreeService = getFamilyTreeService;
   }
 
-  async execute (req: Request, res: Response) {
-    const { id } = req.params
+  async execute(req: Request, res: Response) {
+    const { id } = req.params;
 
-    const result = await this.getFamilyTreeService.execute(id)
+    const result = await this.getFamilyTreeService.execute(id);
 
     if (result.isLeft()) {
-      throw new Error()
+      throw new Error();
     }
-    return result
-  };
+    return result;
+  }
 }
