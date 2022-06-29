ae41a1eb28a255fdd4be019645ede3ee1f080829
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-06-29 13:19:46 -0300

    express router adapter

---
+++ src/main/adapters/express-routes-adapter.ts
@@ -0,0 +1,20 @@
+import { IController, HttpRequest } from '@src/presentation/protocols';
+import { Request, Response } from 'express';
+
+// adapter para qualquer controller ser aceito pelo express, caso seja necessário trocar o express futuramente
+
+export const adaptRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+    };
+    const httpResponse = await controller.handle(httpRequest);
+    if (httpResponse.statusCode === 200) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message,
+      });
+    }
+  };
+};
