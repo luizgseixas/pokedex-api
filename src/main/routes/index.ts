0051b0f232c9772997238480db58a6975eb838be
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-06-29 18:13:26 -0300

    pokemon information route

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { makeGetFamilyTree } from '@src/main/factories/get-family-tree';
+// import { makeGetFamilyTree } from '@src/main/factories/get-family-tree';
 import { adaptRoute } from '../adapters/express-routes-adapter';
+import { makeGetPokemonInformationController } from '../factories/get-pokemon-information';
 import { makeGetPokemonListController } from '../factories/get-pokemon-list';
 
 const router = Router();
 
 router.get('/list', adaptRoute(makeGetPokemonListController()));
-router.get('/pokemon/:name', adaptRoute());
+router.get('/pokemon/:pokemon', adaptRoute(makeGetPokemonInformationController()));
 
 // router.get('/tree/:id', makeGetFamilyTree().execute);
 
