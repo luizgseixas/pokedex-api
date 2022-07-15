3284b2914677a988be0b70bde85f0921ed00a971
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-07-15 16:31:13 -0300

    pokemon informations mock

--- src/data/usecases/__tests__/mocks/get-pokemon-informations.ts
+++ src/data/usecases/__tests__/mocks/get-pokemon-informations.ts
@@ -1,4 +1,6 @@
 import { IPokemonData } from "@src/domain/adapters/responses";
+import { PokemonInformationsModel } from "@src/domain/models/pokemon-informations";
+import { makeFamilyTree } from "./map-family-tree";
 
 export const makePokemonData = (): IPokemonData => ({
   id: 1,
@@ -26,4 +28,29 @@ export const makePokemonData = (): IPokemonData => ({
       url: 'any_url',
     },
   }],
+})
+
+export const makePokemonInformations = (): PokemonInformationsModel => ({
+  id: 1,
+  name: 'any_name',
+  types: [{ type : { name: 'any_type' } }],
+  stats: [{
+    base_stat: 1,
+    stat: { name: 'any_stat' },
+  }],
+  sprites: {
+    front_default: 'any_sprite',
+    front_shiny: 'any_sprite',
+    front_female: 'any_sprite',
+    front_shiny_female: 'any_sprite',
+    back_default: 'any_sprite',
+    back_shiny: 'any_sprite',
+    back_female: 'any_sprite',
+    back_shiny_female: 'any_sprite',
+  },
+  moves: [{
+    name: 'any_name',
+    url: 'any_url',
+  }],
+  familyTree: makeFamilyTree()
 })
