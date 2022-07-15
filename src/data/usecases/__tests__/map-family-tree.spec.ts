83d64bc5cc25fe1d33a6b8a80aea363ecfb2de92
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-07-15 16:52:39 -0300

    test: ensure MapFamilyTree returns only the existing evolutions

---
+++ src/data/usecases/__tests__/map-family-tree.spec.ts
@@ -0,0 +1,117 @@
+import { FamilyTreeRequester } from '@src/domain/adapters';
+import { IEvolutionChain } from '@src/domain/adapters/responses';
+import { left, right } from '@src/domain/shared/utils/either';
+import { IMapFamilyTree } from '@src/domain/usecases';
+import { rejects } from 'assert';
+import { MapFamilyTree } from '../map-family-tree';
+import {
+  makeEvolutionChain,
+  makeFirstAndSecondEvolutionChain,
+  makeFirstEvolutionChain,
+} from './mocks';
+
+const makeFamilyTreeRequester = (): FamilyTreeRequester => {
+  class familyTreeRequesterStub implements FamilyTreeRequester {
+    familyTree(pokemonId: string): Promise<IEvolutionChain> {
+      return new Promise((resolve) => resolve(makeEvolutionChain()));
+    }
+  }
+
+  return new familyTreeRequesterStub();
+};
+
+interface SutTypes {
+  sut: IMapFamilyTree;
+  familyTreeRequesterStub: FamilyTreeRequester;
+}
+
+const sutParam = { pokemonId: '1' };
+
+const makeSut = (): SutTypes => {
+  const familyTreeRequesterStub = makeFamilyTreeRequester();
+  const sut = new MapFamilyTree(familyTreeRequesterStub);
+
+  return {
+    sut,
+    familyTreeRequesterStub,
+  };
+};
+
+describe('MapFamilyTree Usecase', () => {
+  test('Should call familyTreeRequester with correct value', async () => {
+    const { sut, familyTreeRequesterStub } = makeSut();
+    const familyTreeRequesterSpy = jest.spyOn(familyTreeRequesterStub, 'familyTree');
+    await sut.execute(sutParam);
+    expect(familyTreeRequesterSpy).toHaveBeenCalledWith('1');
+  });
+
+  test('Should return familyTreeRequester a left error if familyTreeRequester thword', async () => {
+    const { sut, familyTreeRequesterStub } = makeSut();
+    jest
+      .spyOn(familyTreeRequesterStub, 'familyTree')
+      .mockReturnValueOnce(new Promise((resolve, rejects) => rejects(new Error())));
+    const promise = sut.execute(sutParam);
+    expect(promise).resolves.toEqual(left(Error()));
+  });
+
+  test("Should return only first_evolution if don't have more evolutions on success", async () => {
+    const { sut, familyTreeRequesterStub } = makeSut();
+    jest
+      .spyOn(familyTreeRequesterStub, 'familyTree')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(makeFirstEvolutionChain())));
+    const familyTree = await sut.execute(sutParam);
+    console.log(familyTree);
+    expect(familyTree).toEqual(
+      right({
+        first_evolution: {
+          name: 'any_name',
+        },
+      }),
+    );
+  });
+
+  test("Should return only first_evolution and second_evolution if don't have more evolutions on success", async () => {
+    const { sut, familyTreeRequesterStub } = makeSut();
+    jest
+      .spyOn(familyTreeRequesterStub, 'familyTree')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(makeFirstAndSecondEvolutionChain())));
+    const familyTree = await sut.execute(sutParam);
+    expect(familyTree).toEqual(
+      right({
+        first_evolution: {
+          name: 'any_name',
+        },
+        second_evolution: {
+          evolves_details: {
+            min_level: 16,
+          },
+          name: 'any_name',
+        },
+      }),
+    );
+  });
+
+  test('Should return all three evolutions on success', async () => {
+    const { sut } = makeSut();
+    const familyTree = await sut.execute(sutParam);
+    expect(familyTree).toEqual(
+      right({
+        first_evolution: {
+          name: 'any_name',
+        },
+        second_evolution: {
+          evolves_details: {
+            min_level: 16,
+          },
+          name: 'any_name',
+        },
+        third_evolution: {
+          evolves_details: {
+            min_level: 32,
+          },
+          name: 'any_name',
+        },
+      }),
+    );
+  });
+});
--- src/data/usecases/__tests__/mocks/map-family-tree.ts
+++ src/data/usecases/__tests__/mocks/map-family-tree.ts
@@ -1,4 +1,149 @@
-import { FamilyTreeModel } from "@src/domain/models/family-tree";
+import { IEvolutionChain } from '@src/domain/adapters/responses';
+import { FamilyTreeModel } from '@src/domain/models/family-tree';
+
+export const makeEvolutionChain = (): IEvolutionChain => ({
+  baby_trigger_item: null,
+  chain: {
+    evolution_details: [],
+    evolves_to: [
+      {
+        evolution_details: [
+          {
+            gender: null,
+            held_item: null,
+            item: null,
+            known_move: null,
+            known_move_type: null,
+            location: null,
+            min_affection: null,
+            min_beauty: null,
+            min_happiness: null,
+            min_level: 16,
+            needs_overworld_rain: false,
+            party_species: null,
+            party_type: null,
+            relative_physical_stats: null,
+            time_of_day: '',
+            trade_species: null,
+            trigger: {
+              name: 'level-up',
+              url: 'any_url',
+            },
+            turn_upside_down: false,
+          },
+        ],
+        evolves_to: [
+          {
+            evolution_details: [
+              {
+                gender: null,
+                held_item: null,
+                item: null,
+                known_move: null,
+                known_move_type: null,
+                location: null,
+                min_affection: null,
+                min_beauty: null,
+                min_happiness: null,
+                min_level: 32,
+                needs_overworld_rain: false,
+                party_species: null,
+                party_type: null,
+                relative_physical_stats: null,
+                time_of_day: '',
+                trade_species: null,
+                trigger: {
+                  name: 'level-up',
+                  url: 'any_url',
+                },
+                turn_upside_down: false,
+              },
+            ],
+            evolves_to: [],
+            is_baby: false,
+            species: {
+              name: 'any_name',
+              url: 'any_url',
+            },
+          },
+        ],
+        is_baby: false,
+        species: {
+          name: 'any_name',
+          url: 'any_url',
+        },
+      },
+    ],
+    is_baby: false,
+    species: {
+      name: 'any_name',
+      url: 'any_url',
+    },
+  },
+  id: 1,
+});
+
+export const makeFirstEvolutionChain = (): IEvolutionChain => ({
+  baby_trigger_item: null,
+  chain: {
+    evolution_details: [],
+    evolves_to: [],
+    is_baby: false,
+    species: {
+      name: 'any_name',
+      url: 'any_url',
+    },
+  },
+  id: 1,
+});
+
+export const makeFirstAndSecondEvolutionChain = (): IEvolutionChain => ({
+  baby_trigger_item: null,
+  chain: {
+    evolution_details: [],
+    evolves_to: [
+      {
+        evolution_details: [
+          {
+            gender: null,
+            held_item: null,
+            item: null,
+            known_move: null,
+            known_move_type: null,
+            location: null,
+            min_affection: null,
+            min_beauty: null,
+            min_happiness: null,
+            min_level: 16,
+            needs_overworld_rain: false,
+            party_species: null,
+            party_type: null,
+            relative_physical_stats: null,
+            time_of_day: '',
+            trade_species: null,
+            trigger: {
+              name: 'level-up',
+              url: 'any_url',
+            },
+            turn_upside_down: false,
+          },
+        ],
+        evolves_to: [],
+        is_baby: false,
+        species: {
+          name: 'any_name',
+          url: 'any_url',
+        },
+      },
+    ],
+    is_baby: false,
+    species: {
+      name: 'any_name',
+      url: 'any_url',
+    },
+  },
+  id: 1,
+});
 
 export const makeFamilyTree = (): FamilyTreeModel => ({
   first_evolution: {
@@ -6,11 +151,11 @@ export const makeFamilyTree = (): FamilyTreeModel => ({
   },
   second_evolution: {
     name: 'any_name',
-    evolves_details: {},
+    evolves_details: { min_level: 16 },
   },
 
   third_evolution: {
     name: 'any_name',
-    evolves_details: {},
+    evolves_details: { min_level: 32 },
   },
 });
