d46e352a21cea6f7c9664766c2bd307d627421a7
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-07-15 19:06:10 -0300

    chore: jest use dotenv

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootoDir>/src/main/**'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootoDir>/src/main/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest',
   },
-  "moduleNameMapper": {
-    "^@src/(.*)$": "<rootDir>/src/$1"
-  }
-}
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFiles: ['dotenv/config'],
+};
