daeb116ebaa45e12aa6577a1af9938a8b58c5113
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-07-15 17:38:39 -0300

    refactor: rename mock files

---
+++ src/data/usecases/__tests__/mocks/get-pokemons-list.mock.ts
@@ -0,0 +1,89 @@
+import { IPokemonListResponse } from '@src/domain/adapters/responses';
+
+export const makePrimitivePokemonsList = (): IPokemonListResponse => ({
+  count: 1154,
+  next: 'any_url',
+  previous: null,
+  results: [
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+    {
+      name: 'any_name',
+      url: 'any_url',
+    },
+  ],
+});
