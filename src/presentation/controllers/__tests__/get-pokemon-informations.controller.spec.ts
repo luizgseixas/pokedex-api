099fd523a7bb4c30825a0ff2dabaaf8cf9e910c3
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-08-06 11:25:40 -0300

    test: GetPokemonInformations Controller unit test

--- src/presentation/controllers/__tests__/get-pokemon-informations.controller.spec.ts
+++ src/presentation/controllers/__tests__/get-pokemon-informations.controller.spec.ts
@@ -1,21 +1,26 @@
 import { GetPokemonInformationsController } from '../get-pokemon-informations.controller';
-import { IController } from '../../protocols/controller';
 import { IGetPokemonInformations } from '../../../domain/usecases';
-import { makePokemonInformations } from './__mocks__/get-pokemons-informations.mock';
-import { right } from '../../../domain/shared/utils/either';
+import { makePokemonInformations } from './__mocks__/get-pokemon-informations.mock';
+import { failure, success } from '../../../domain/shared/utils/either';
+import { HttpRequest } from '../../protocols';
+import { ok, serverError } from '../../helpers/http-helper';
 
 const makeGetPokemonInformations = () => {
   class GetPokemonInformationsStub implements IGetPokemonInformations {
-    async execute(params: IGetPokemonInformations.Params): IGetPokemonInformations.Result {
-      return new Promise((resolve) => resolve(right(makePokemonInformations())));
+    async execute (params: IGetPokemonInformations.Params): IGetPokemonInformations.Result {
+      return new Promise((resolve) => resolve(success(makePokemonInformations())));
     }
   }
 
   return new GetPokemonInformationsStub();
 };
 
+const makeHttpRequest = (): HttpRequest => ({
+  params: { pokemonId: '1' },
+});
+
 interface SutTypes {
-  sut: IController,
+  sut: GetPokemonInformationsController,
   getPokemonInformationsStub: IGetPokemonInformations
 }
 
@@ -32,5 +37,23 @@ const makeSut = (): SutTypes => {
 describe('GetPokemonInformations Controller', () => {
   test('Should call GetPokemonInformations with correct value', async () => {
     const { sut, getPokemonInformationsStub } = makeSut();
+    const infoSpy = jest.spyOn(getPokemonInformationsStub, 'execute');
+    await sut.handle(makeHttpRequest());
+    expect(infoSpy).toHaveBeenCalledWith({ pokemonId: '1' });
+  });
+
+  test('Should returns 500 if GetPokemonInformations throws', async () => {
+    const { sut, getPokemonInformationsStub } = makeSut();
+    jest.spyOn(getPokemonInformationsStub, 'execute').mockReturnValueOnce(
+      new Promise(((resolve) => resolve(failure(new Error())))),
+    );
+    const promise = sut.handle(makeHttpRequest());
+    await expect(promise).resolves.toEqual(serverError(new Error()));
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeHttpRequest());
+    expect(httpResponse).toEqual(ok(makePokemonInformations()));
   });
 });
