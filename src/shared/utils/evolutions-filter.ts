a0e99f39e4de5da6bce84d4077ed55e23383cc8c
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-08-17 12:41:49 -0300

    feat: filter evolutions on the chain object

--- src/shared/utils/evolutions-filter.ts
+++ src/shared/utils/evolutions-filter.ts
@@ -1,4 +1,7 @@
-export const evolutionsFilter = (arr: Array<any>) => {
+import { IChain } from '@src/domain/adapters/responses';
+import { FamilyTreeModel } from '@src/domain/models/family-tree';
+
+export const evolutionsDetailsFilter = (arr: Array<any>) => {
   const evolutions = arr.map((datail) => Object.keys(datail).reduce((newObj, key) => {
     const value = datail[key];
     if (value !== null && value !== false && key !== 'trigger' && value !== '') {
@@ -6,6 +9,45 @@ export const evolutionsFilter = (arr: Array<any>) => {
     }
     return newObj;
   }, {}))[0];
+
+  return evolutions;
+};
+
+let evolutions = {};
+
+export const chainFilter = (chain: IChain) => {
+  let count = 1;
+
+  evolutions[`evolution_${count}`] = { name: chain.species.name };
+
+  console.log(chain);
+
+  chain.evolves_to.forEach((evolution, index) => {
+    count += 1;
+    const details = evolutionsDetailsFilter(chain.evolves_to[index].evolution_details);
+    evolutions[`evolution_${count}`] = { name: evolution.species.name, evolution_details: details };
+
+    if (chain.evolves_to[index].evolves_to.length > 0) {
+      const lastChain = chainFilter(chain.evolves_to[index].evolves_to[0]);
+      if (lastChain) {
+        evolutions[`evolution_${count}`] = lastChain;
+      }
+    }
+  });
+
+  // const evolves = chain.evolves_to.reduce((newObj, current, key) => {
+  //   if (chain.evolves_to.length > 0) {
+  //     count += 1;
+
+  //     const details = evolutionsDetailsFilter(chain.evolves_to[key].evolution_details);
+  //     const lastChain = chainFilter(chain.evolves_to[key]);
+  //     newObj[`evolution_${count}`] = { name: current.species.name, evolution_details: details };
+  //     if (lastChain) {
+  //       newObj[`evolution_${count}`] = lastChain;
+  //     }
+  //   }
+  //   return newObj;
+  // }, {});
 
   return evolutions;
 };
