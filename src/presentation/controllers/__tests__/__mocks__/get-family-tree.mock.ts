1fd3b2ef599d157e5b08cb1760df249605895950
Luiz_Seixas <ticoluizgustavo@outlook.com>
2022-08-17 19:56:03 -0300

    test: change all controllers unit tests to use new pokemonFamilyTree format

--- src/presentation/controllers/__tests__/__mocks__/get-family-tree.mock.ts
+++ src/presentation/controllers/__tests__/__mocks__/get-family-tree.mock.ts
@@ -1,16 +1,25 @@
-import { FamilyTreeModel } from "@src/domain/models/family-tree";
+import { FamilyTreeModel } from '@src/domain/models/family-tree';
 
-export const makeFamilyTree = (): FamilyTreeModel => ({
-  first_evolution: {
+export const makeFamilyTree = (): FamilyTreeModel[] => ([
+  {
+    evolution_lvl: 1,
     name: 'any_name',
+    evolution_details: null,
   },
-  second_evolution: {
+  {
+    evolution_lvl: 2,
     name: 'any_name',
-    evolves_details: { min_level: 16 },
+    evolution_details: {
+      min_level: 16,
+      trigger: 'level-up',
+    },
   },
-
-  third_evolution: {
+  {
+    evolution_lvl: 3,
     name: 'any_name',
-    evolves_details: { min_level: 32 },
+    evolution_details: {
+      min_level: 32,
+      trigger: 'level-up',
+    },
   },
-})
+]);
