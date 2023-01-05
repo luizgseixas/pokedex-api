61addb9cbc38eabfa75701b2a53fd23e1cc3075e
Luiz_Seixas <ticoluizgustavo@outlook.com>
2023-01-05 15:12:03 -0300

    refactor: rename typeModel

--- src/domain/models/pokemon/type.ts
+++ src/domain/models/pokemon/type.ts
@@ -1,7 +1,7 @@
 import { Pokemon } from './pokemon';
 
-export class Type {
-  constructor (data?: Omit<Type, 'id'>, id?: string) {
+export class TypeModel {
+  constructor (data?: Omit<TypeModel, 'id'>, id?: string) {
     if (data) Object.assign(this, data);
     if (id) this.id = id;
   }
