d8f764276bb485728931350c621d14b7a29dd820
Luiz Seixas <ticoluizgustavo@outlook.com>
2023-02-20 11:59:41 -0300

    test: ensure FindPokemonByName returns an pokemon on success

--- src/data/usecases/pokemon/find-pokemon/find-pokemon-by-name.spec.ts
+++ src/data/usecases/pokemon/find-pokemon/find-pokemon-by-name.spec.ts
@@ -1,5 +1,5 @@
 import { PokemonModel } from '@src/domain/models/pokemon';
-import { failure } from '@src/domain/shared/utils/either';
+import { failure, success } from '@src/domain/shared/utils/either';
 import { throwError } from '@src/domain/test';
 import { mockPokemon } from '@src/domain/test/mock-pokemon';
 import { IFindPokemonByName } from '@src/domain/usecases/pokemon/find-pokemon-by-name';
@@ -20,7 +20,7 @@ export class FindPokemonByNameUseCase {
   async execute (params: IFindPokemonByName.Params): Promise<any> {
     try {
       const pokemon = await this.pokemonRepository.findByName(params.name);
-      return pokemon;
+      return success(pokemon);
     } catch (err) {
       console.error(err);
       return failure(err);
@@ -48,16 +48,16 @@ describe('FindPokemonByNameUseCase', () => {
     const { sut, findPokemonByNameRepositoryStub } = makeSut();
     const findSpy = jest.spyOn(findPokemonByNameRepositoryStub, 'findByName');
 
-    await sut.execute({ name: 'Charmander' });
+    await sut.execute({ name: 'any_name' });
 
-    expect(findSpy).toHaveBeenCalledWith('Charmander');
+    expect(findSpy).toHaveBeenCalledWith('any_name');
   });
 
   test('sould retusn a failure error if FindPokemonByNameRepository throws', async () => {
     const { sut, findPokemonByNameRepositoryStub } = makeSut();
     jest.spyOn(findPokemonByNameRepositoryStub, 'findByName').mockImplementationOnce(throwError);
 
-    const promise = sut.execute({ name: 'Charmander' });
+    const promise = sut.execute({ name: 'any_name' });
 
     expect(promise).resolves.toEqual(failure(Error()));
   });
@@ -66,8 +66,16 @@ describe('FindPokemonByNameUseCase', () => {
     const { sut, findPokemonByNameRepositoryStub } = makeSut();
     jest.spyOn(findPokemonByNameRepositoryStub, 'findByName').mockResolvedValueOnce(null);
 
-    const pokemon = await sut.execute({ name: 'Charmander' });
+    const pokemon = await sut.execute({ name: 'any_name' });
 
-    expect(pokemon).toBeNull();
+    expect(pokemon.value).toBeNull();
+  });
+
+  test('sould returns an pokemon on success', async () => {
+    const { sut } = makeSut();
+
+    const pokemon = await sut.execute({ name: 'any_name' });
+
+    expect(pokemon).toEqual(success(mockPokemon()));
   });
 });
