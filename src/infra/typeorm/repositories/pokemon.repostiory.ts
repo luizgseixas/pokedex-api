5887c680f528431b5d06326e27746b1e2d4e9445
Luiz Seixas <ticoluizgustavo@outlook.com>
2023-02-20 12:12:27 -0300

    feat: FindPokemonByName repository

--- src/infra/typeorm/repositories/pokemon.repostiory.ts
+++ src/infra/typeorm/repositories/pokemon.repostiory.ts
@@ -1,9 +1,18 @@
-import { ISavePokemonRepository } from '@src/data/contracts/db/pokemon/save-pokemon';
+import { ISavePokemonRepository, IFindPokemonByNameRepository } from '@src/data/contracts/db/pokemon';
+import { PokemonModel } from '@src/domain/models/pokemon';
 import { Repository } from 'typeorm';
 import { PokemonsEntity } from '../entities';
 
-export class PokemonsRepository implements ISavePokemonRepository {
+export class PokemonsRepository implements ISavePokemonRepository, IFindPokemonByNameRepository {
   constructor (private readonly pokemonsRepository: Repository<PokemonsEntity>) {}
+
+  async findByName (name: string): Promise<PokemonModel> {
+    return this.pokemonsRepository.findOne({
+      where: {
+        name,
+      },
+    });
+  }
 
   async save (params: ISavePokemonRepository.Params): ISavePokemonRepository.Result {
     const pokemon = this.pokemonsRepository.create(params);
